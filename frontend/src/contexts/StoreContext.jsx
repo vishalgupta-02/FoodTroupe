f44d947b6e3b956f5976d16aebe31dcf64f59b85
Vishal <abhimanyug987@gmail.com>
2025-01-05 12:02:18 +0000

    2nd day (Some files created)

--- frontend/src/contexts/StoreContext.jsx
+++ frontend/src/contexts/StoreContext.jsx
@@ -1,18 +1,28 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 // import PropTypes from 'prop-types'
+import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        if(!cartItems[itemId]){
+            setCartItems((prev)=> ({...prev, [itemId]:1}))
+        } else{
+            setCartItems((prev)=> ({...prev, [itemId]: prev[itemId]+1}))
+        }
+    }
     
     const contextValue = {
-
+        food_list,
     }
     return(
-        <StoreContextProvider value={contextValue}>
+        <StoreContext.Provider value={contextValue}>
             {props.children}
-        </StoreContextProvider>
+        </StoreContext.Provider>
     )
 }
 
